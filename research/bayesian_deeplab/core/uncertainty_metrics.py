2b82e07bc67eea788f293747efb0f14bb2dd35bc
Jishnu Mukhoti <ms17jm2@tw04.tcs.cs.ox.ac.uk>
2018-05-24 18:47:56 +0100

    Tested a naive method to generate variation ratios as an uncertainty metric

--- research/bayesian_deeplab/core/uncertainty_metrics.py
+++ research/bayesian_deeplab/core/uncertainty_metrics.py
@@ -16,7 +16,7 @@ _MUTUAL_INFORMATION = 'mutual_information'
 
 
 def mean_prediction(monte_carlo_samples):
-  """ Produces the mean of the given Monte Carlo samples as the
+  """ Produces the mean of the given Monte Carlo samples.
 
   Args:
     monte_carlo_samples: A list of predicted tensors where each element in the list
@@ -24,7 +24,38 @@ def mean_prediction(monte_carlo_samples):
 
   Returns:
     The mean of the predictions. The dimension of this tensor is [batch, height, width, num_classes].
+    In order to get the actual predictions of the classes, an argmax operation is required on the
+    channels.
   """
   return tf.reduce_mean(monte_carlo_samples, 0)
+
+
+def variation_ratio(monte_carlo_samples):
+  """ Produces the pixel-wise variation ratio of the given Monte Carlo samples as an uncertainty metric.
+
+  Args:
+    monte_carlo_samples: A list of predicted tensors where each element in the list
+                         has dimensions [batch, height, width, num_classes].
+
+  Returns:
+    The variation ratio generated from the Monte Carlo samples. The dimension of the output tensor is
+    [batch, height, width, 1]. There is just one channel for each image where each pixel contains the
+    variation-ratio obtained from the MC samples.
+  """
+  num_mc_trials = len(monte_carlo_samples)
+
+  # Getting the tensor shape
+  shape = monte_carlo_samples[0].get_shape().as_list()
+  batch = shape[0]
+
+  # Modified MC samples is a list where each element has dimensions [batch, height, width, 1]. Each element
+  # in the single channel actually represents the class to which the pixel has been classified.
+  modified_mc_samples = []
+  for i in range(0, num_mc_trials):
+    modified_mc_samples[i] = tf.argmax(monte_carlo_samples[i], 3)
+
+  # Following tensor has dimensions [batch, height, width, num_mc_trials]
+  mod_tensor = tf.concat(modified_mc_samples, axis=3)
+
 
 
